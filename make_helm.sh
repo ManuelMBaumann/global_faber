760308ebc640097e96b6b650f9e2b60df5b7c6c4
ManuelMBaumann <manuelmbaumann@gmail.com>
2017-02-02 09:30:14 +0100

    add driver

---
+++ make_helm.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+. ~/.bashrc
+
+python3 helm_wedge.py --ndims=2 --dx=10 --dy=10 --dz=10 --freq=[1,5] --Nom=5 --degree=1 --damping=0.0 storing=True --plots=True --nprocs=4                              
+                            
+                            
+                            
+                            
+
+           
